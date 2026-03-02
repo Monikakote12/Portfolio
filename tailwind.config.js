a45f3da0314519f49e388165cd890a58e2a00f17
Monikakote12 <monikakote12@gmail.com>
2026-03-02 18:16:22 +0530

    portfolio using tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease in forward",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": {opacity: "0", transform:"translateY(20px)"},
+          "100%": {opacity:"1", transform:"translateY(0)"},
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
